ec031f2120bacd2190fedc1701086dfee2bf8f9b
Абдималиков Байэл Эрнистович <b.abdimalikov@mkit.local>
2025-01-13 11:38:46 +0300

    fix: function odd_even

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 console.log("Hello world!")
 function odd_even(number){
-    if(number%2==0) return(number+" это нечётное число")
-    else return(number+" это чётное число")
+    if(number%2==0) return(number+" это чётное число")
+    else return(number+" это нечётное число")
 }
 console.log(odd_even(4))
