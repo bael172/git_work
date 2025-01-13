c8e34d92b0c2e4b7c3aafcd1ae92b656552bcbf0
Абдималиков Байэл Эрнистович <b.abdimalikov@mkit.local>
2025-01-13 11:31:26 +0300

    feat!: added function calculating odd or even number BREAKING CHANGE: function is working wrong

---
+++ code.js
@@ -0,0 +1,6 @@
+console.log("Hello world!")
+function odd_even(number){
+    if(number%2==0) return(number+" это нечётное число")
+    else return(number+" это чётное число")
+}
+console.log(odd_even(4))
